e1d35ac4af9c4a3e964f30b389f05f9d54e51caa
Justin Miller <indymiller@gmail.com>
2020-06-26 12:51:55 -0400

    add new food form added

--- food/urls.py
+++ food/urls.py
@@ -4,4 +4,5 @@ from food import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('generic_form.html', views.add_food, name='addfood'),
 ]
--- food/views.py
+++ food/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from food.models import Food
+from food.forms import AddFoodForm
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html',
                   {'nodes': Food.objects.all()})
+
+def add_food(request):
+    if request.method == 'POST':
+        form = AddFoodForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = AddFoodForm()
+    return render(request, 'generic_form.html', {'form': form})
